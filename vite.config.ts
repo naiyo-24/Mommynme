2e6beed45145d6aab34acbaf95eb983c36189da1
Soumik Hazra <soumikhazra100@gmail.com>
2025-04-19 16:28:17 +0530

    Add Dockerfile and docker-compose.yml for containerization; update vite.config.ts for server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,18 @@ export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.png'], // Add PNG to assets
   server: {
+    host: '0.0.0.0', // Expose the server to the network
+    port: 4173, // Set the server to run on port 3000
     proxy: {
       '/api': {
         target: 'https://frappe-client1.sirfbill.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        rewrite: (path: string) => path.replace(/^\/api/, '')
       }
     }
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
   }
 });
